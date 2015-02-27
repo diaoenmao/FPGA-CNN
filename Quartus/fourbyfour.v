b147a8b5948ecfc0e3f534a23fc4c9ab46c08d36
DreamSoul <diao_em@hotmail.com>
2015-02-27 00:28:12 -0500

    One more level for eliminating input ports

--- Quartus/fourbyfour.v
+++ Quartus/fourbyfour.v
@@ -1,11 +1,11 @@
-module main (
+module fourbyfour (
 A1, A2, A3, A4, A5, A6, A7, A8, A9,
     
 B1, B2, B3, B4, B5, B6, B7, B8, B9,
     
-U1, U2, U3, U4, U5, U6, U7, U8, U9,
+U1_in, U2_in, U3_in, U4_in, U5_in, U6_in, U7_in, U8_in, U9_in, U10_in, U11_in, U12_in, U13_in, U14_in, U15_in, U16_in,
 	
-I,
+I1, I2, I3, I4, I5, I6, I7, I8, I9, I10, I11, I12, I13, I14, I15, I16,
 
 clk,
 
@@ -14,13 +14,13 @@ Y1_out, Y2_out, Y3_out, Y4_out, Y5_out, Y6_out, Y7_out, Y8_out, Y9_out, Y10_out,
 	input wire clk;
 	input wire [7:0] A1, A2, A3, A4, A5, A6, A7, A8, A9;
 	input wire [7:0] B1, B2, B3, B4, B5, B6, B7, B8, B9;
-	input wire [7:0] U1, U2, U3, U4, U5, U6, U7, U8, U9;
-	input wire [7:0] I;
+	input wire [7:0] U1_in, U2_in, U3_in, U4_in, U5_in, U6_in, U7_in, U8_in, U9_in, U10_in, U11_in, U12_in, U13_in, U14_in, U15_in, U16_in;
+	input wire [7:0] I1, I2, I3, I4, I5, I6, I7, I8, I9, I10, I11, I12, I13, I14, I15, I16;
 	reg [7:0] A1i, A2i, A3i, A4i, A5i, A6i, A7i, A8i, A9i;
 	reg [7:0] B1i, B2i, B3i, B4i, B5i, B6i, B7i, B8i, B9i;
-	reg [7:0] U1i, U2i, U3i, U4i, U5i, U6i, U7i, U8i, U9i;
+	reg [7:0] U1, U2, U3, U4, U5, U6, U7, U8, U9, U10, U11, U12, U13, U14, U15, U16;
 	reg signed [8:0] Y1, Y2, Y3, Y4, Y5, Y6, Y7, Y8, Y9, Y10, Y11, Y12, Y13, Y14, Y15, Y16;
-	reg [7:0] Ii;
+	reg [7:0] I;
 	reg [3:0] counter;
 	wire signed [8:0] onebyoneout;
 	reg signed [8:0] Y1_next, Y2_next, Y3_next, Y4_next, Y5_next, Y6_next, Y7_next, Y8_next, Y9_next, Y10_next, Y11_next, Y12_next, Y13_next, Y14_next, Y15_next, Y16_next;
@@ -42,7 +42,7 @@ assign Y13_out = Y13_next;
 assign Y14_out = Y14_next;
 assign Y15_out = Y15_next;
 assign Y16_out = Y16_next;
-	
+
 always  @ (posedge clk)
 begin
   A1i <= A1;
@@ -65,73 +65,161 @@ begin
   B8i <= B8;
   B9i <= B9;
   
-  U1i <= U1;
-  U2i <= U2;
-  U3i <= U3;
-  U4i <= U4;
-  U5i <= U5;
-  U6i <= U6;
-  U7i <= U7;
-  U8i <= U8;
-  U9i <= U9;
+  U1 <= U1_in;
+  U2 <= U2_in;
+  U3 <= U3_in;
+  U4 <= U4_in;
+  U5 <= U5_in;
+  U6 <= U6_in;
+  U7 <= U7_in;
+  U8 <= U8_in;
+  U9 <= U9_in;
+  U10 <= U10_in;
+  U11 <= U11_in;
+  U12 <= U12_in;
+  U13 <= U13_in;
+  U14 <= U14_in;
+  U15 <= U15_in;
+  U16 <= U16_in;
   
-  Ii <= I;
   if (counter == 4'b0000) begin
-    Y1 <= 8'b00000000;
-    Y2 <= 8'b00000000;
-    Y3 <= 8'b00000000;
-    Y4 <= 8'b00000000;
+  
+	U1 <= 8'b0;
+	U2 <= 8'b0;
+	U3 <= 8'b0;
+	U4 <= 8'b0;
+	U5 <= U1;
+	U6 <= U2;
+	U7 <= 8'b0;
+	U8 <= U5;
+	U9 <= U6;
+  
+    Y1 <= 8'b0;
+    Y2 <= 8'b0;
+    Y3 <= 8'b0;
+    Y4 <= 8'b0;
     Y5 <= Y1;
     Y6 <= Y2;
-    Y7 <= 8'b00000000;
+    Y7 <= 8'b0;
     Y8 <= Y5;
     Y9 <= Y6;   
+    
+    I <= I1;
+    
     Y1_next <= onebyoneout;
+    
   end else if (counter == 4'b0001) begin
-    Y1 <= 8'b00000000;
-    Y2 <= 8'b00000000;
-    Y3 <= 8'b00000000;
+  
+    U1 <= 8'b0;
+    U2 <= 8'b0;
+    U3 <= 8'b0;
+    U4 <= U1;
+    U5 <= U2;
+    U6 <= U3;
+    U7 <= U5;
+    U8 <= U6;
+    U9 <= U7;
+    
+    Y1 <= 8'b0;
+    Y2 <= 8'b0;
+    Y3 <= 8'b0;
     Y4 <= Y1;
     Y5 <= Y2;
     Y6 <= Y3;
     Y7 <= Y5;
     Y8 <= Y6;
     Y9 <= Y7; 
+    
+    I <= I2;
+        
     Y2_next <= onebyoneout;
   end else if (counter == 4'b0010) begin
-    Y1 <= 8'b00000000;
-    Y2 <= 8'b00000000;
-    Y3 <= 8'b00000000;
+  
+    U1 <= 8'b0;
+    U2 <= 8'b0;
+    U3 <= 8'b0;
+    U4 <= U2;
+    U5 <= U3;
+    U6 <= U4;
+    U7 <= U6;
+    U8 <= U7;
+    U9 <= U8;
+    
+    Y1 <= 8'b0;
+    Y2 <= 8'b0;
+    Y3 <= 8'b0;
     Y4 <= Y2;
     Y5 <= Y3;
     Y6 <= Y4;
     Y7 <= Y6;
     Y8 <= Y7;
     Y9 <= Y8;
+    
+    I <= I3;
+    
     Y3_next <= onebyoneout; 
   end else if (counter == 4'b0011) begin
-    Y1 <= 8'b00000000;
-    Y2 <= 8'b00000000;
-    Y3 <= 8'b00000000;
+  
+    U1 <= 8'b0;
+    U2 <= 8'b0;
+    U3 <= 8'b0;
+    U4 <= U3;
+    U5 <= U4;
+    U6 <= 8'b0;
+    U7 <= U7;
+    U8 <= U8;
+    U9 <= 8'b0;
+    
+    Y1 <= 8'b0;
+    Y2 <= 8'b0;
+    Y3 <= 8'b0;
     Y4 <= Y3;
     Y5 <= Y4;
-    Y6 <= 8'b00000000;
+    Y6 <= 8'b0;
     Y7 <= Y7;
     Y8 <= Y8;
-    Y9 <= 8'b00000000;
+    Y9 <= 8'b0;
+    
+    I <= I4;
+    
     Y4_next <= onebyoneout;
   end else if (counter == 4'b0100) begin
-    Y1 <= 8'b00000000;
+  
+    U1 <= 8'b0;
+    U2 <= U1;
+    U3 <= U2;
+    U4 <= 8'b0;
+    U5 <= U5;
+    U6 <= U6;
+    U7 <= 8'b0;
+    U8 <= U9;
+    U9 <= U10;
+    
+    Y1 <= 8'b0;
     Y2 <= Y1;
     Y3 <= Y2;
-    Y4 <= 8'b00000000;
+    Y4 <= 8'b0;
     Y5 <= Y5;
     Y6 <= Y6;
-    Y7 <= 8'b00000000;
+    Y7 <= 8'b0;
     Y8 <= Y9;
     Y9 <= Y10;
+    
+    I <= I5;
+    
     Y5_next <= onebyoneout;
   end else if (counter == 4'b0101) begin
+  
+    U1 <= U1;
+    U2 <= U2;
+    U3 <= U3;
+    U4 <= U5;
+    U5 <= U6;
+    U6 <= U7;
+    U7 <= U9;
+    U8 <= U10;
+    U9 <= U11;
+    
     Y1 <= Y1;
     Y2 <= Y2;
     Y3 <= Y3;
@@ -141,8 +229,22 @@ begin
     Y7 <= Y9;
     Y8 <= Y10;
     Y9 <= Y11;
+    
+    I <= I6;
+    
     Y6_next <= onebyoneout;
   end else if (counter == 4'b0110) begin
+  
+    U1 <= U2;
+    U2 <= U3;
+    U3 <= U4;
+    U4 <= U6;
+    U5 <= U7;
+    U6 <= U8;
+    U7 <= U10;
+    U8 <= U11;
+    U9 <= U12;
+    
     Y1 <= Y2;
     Y2 <= Y3;
     Y3 <= Y4;
@@ -152,30 +254,72 @@ begin
     Y7 <= Y10;
     Y8 <= Y11;
     Y9 <= Y12;
+    
+    I <= I7;
+    
     Y7_next <= onebyoneout;
   end else if (counter == 4'b0111) begin
+  
+    U1 <= U3;
+    U2 <= U4;
+    U3 <= 8'b0;
+    U4 <= U7;
+    U5 <= U8;
+    U6 <= 8'b0;
+    U7 <= U11;
+    U8 <= U12;
+    U9 <= 8'b0;
+
     Y1 <= Y3;
     Y2 <= Y4;
-    Y3 <= 8'b00000000;
+    Y3 <= 8'b0;
     Y4 <= Y7;
     Y5 <= Y8;
-    Y6 <= 8'b00000000;
+    Y6 <= 8'b0;
     Y7 <= Y11;
     Y8 <= Y12;
-    Y9 <= 8'b00000000;
+    Y9 <= 8'b0;
+    
+    I <= I8;
+    
     Y8_next <= onebyoneout;
   end else if (counter == 4'b1000) begin
-    Y1 <= 8'b00000000;
+  
+    U1 <= 8'b0;
+    U2 <= U5;
+    U3 <= U6;
+    U4 <= 8'b0;
+    U5 <= U9;
+    U6 <= U10;
+    U7 <= 8'b0;
+    U8 <= U13;
+    U9 <= U14;
+    
+    Y1 <= 8'b0;
     Y2 <= Y5;
     Y3 <= Y6;
-    Y4 <= 8'b00000000;
+    Y4 <= 8'b0;
     Y5 <= Y9;
     Y6 <= Y10;
-    Y7 <= 8'b00000000;
+    Y7 <= 8'b0;
     Y8 <= Y13;
     Y9 <= Y14;
+    
+    I <= I9;
+    
     Y9_next <= onebyoneout;
   end else if (counter == 4'b1001) begin
+  
+    U1 <= U5;
+    U2 <= U6;
+    U3 <= U7;
+    U4 <= U9;
+    U5 <= U10;
+    U6 <= U11;
+    U7 <= U13;
+    U8 <= U14;
+    U9 <= U15;
+ 
     Y1 <= Y5;
     Y2 <= Y6;
     Y3 <= Y7;
@@ -185,8 +329,22 @@ begin
     Y7 <= Y13;
     Y8 <= Y14;
     Y9 <= Y15;
+    
+    I <= I10;
+    
     Y10_next <= onebyoneout;
   end else if (counter == 4'b1010) begin
+  
+    U1 <= U6;
+    U2 <= U7;
+    U3 <= U8;
+    U4 <= U10;
+    U5 <= U11;
+    U6 <= U12;
+    U7 <= U14;
+    U8 <= U15;
+    U9 <= U16;
+  
     Y1 <= Y6;
     Y2 <= Y7;
     Y3 <= Y8;
@@ -196,61 +354,134 @@ begin
     Y7 <= Y14;
     Y8 <= Y15;
     Y9 <= Y16;
+    
+    I <= I11;
+    
     Y11_next <= onebyoneout;
   end else if (counter == 4'b1011) begin
+  
+    U1 <= U7;
+    U2 <= U8;
+    U3 <= 8'b0;
+    U4 <= U11;
+    U5 <= U12;
+    U6 <= 8'b0;
+    U7 <= U15;
+    U8 <= U16;
+    U9 <= 8'b0;
+    
     Y1 <= Y7;
     Y2 <= Y8;
-    Y3 <= 8'b00000000;
+    Y3 <= 8'b0;
     Y4 <= Y11;
     Y5 <= Y12;
-    Y6 <= 8'b00000000;
+    Y6 <= 8'b0;
     Y7 <= Y15;
     Y8 <= Y16;
-    Y9 <= 8'b00000000;
+    Y9 <= 8'b0;
+    
+    I <= I12;
+    
     Y12_next <= onebyoneout;
   end else if (counter == 4'b1100) begin
-    Y1 <= 8'b00000000;
+  
+    U1 <= 8'b0;
+    U2 <= U9;
+    U3 <= U10;
+    U4 <= 8'b0;
+    U5 <= U13;
+    U6 <= U14;
+    U7 <= 8'b0;
+    U8 <= 8'b0;
+    U9 <= 8'b0;
+    
+    Y1 <= 8'b0;
     Y2 <= Y9;
     Y3 <= Y10;
-    Y4 <= 8'b00000000;
+    Y4 <= 8'b0;
     Y5 <= Y13;
     Y6 <= Y14;
-    Y7 <= 8'b00000000;
-    Y8 <= 8'b00000000;
-    Y9 <= 8'b00000000;
+    Y7 <= 8'b0;
+    Y8 <= 8'b0;
+    Y9 <= 8'b0;
+    
+    I <= I13;
+    
     Y13_next <= onebyoneout;
   end else if (counter == 4'b1101) begin
+  
+    U1 <= U9;
+    U2 <= U10;
+    U3 <= U11;
+    U4 <= U13;
+    U5 <= U14;
+    U6 <= U15;
+    U7 <= 8'b0;
+    U8 <= 8'b0;
+    U9 <= 8'b0;
+    
     Y1 <= Y9;
     Y2 <= Y10;
     Y3 <= Y11;
     Y4 <= Y13;
     Y5 <= Y14;
     Y6 <= Y15;
-    Y7 <= 8'b00000000;
-    Y8 <= 8'b00000000;
-    Y9 <= 8'b00000000;
+    Y7 <= 8'b0;
+    Y8 <= 8'b0;
+    Y9 <= 8'b0;
+    
+    I <= I14;
+    
     Y14_next <= onebyoneout;
   end else if (counter == 4'b1110) begin
+  
+    U1 <= U10;
+    U2 <= U11;
+    U3 <= U12;
+    U4 <= U14;
+    U5 <= U15;
+    U6 <= U16;
+    U7 <= 8'b0;
+    U8 <= 8'b0;
+    U9 <= 8'b0;
+    
     Y1 <= Y10;
     Y2 <= Y11;
     Y3 <= Y12;
     Y4 <= Y14;
     Y5 <= Y15;
     Y6 <= Y16;
-    Y7 <= 8'b00000000;
-    Y8 <= 8'b00000000;
-    Y9 <= 8'b00000000;
+    Y7 <= 8'b0;
+    Y8 <= 8'b0;
+    Y9 <= 8'b0;
+    
+    I <= I15;
+    
     Y15_next <= onebyoneout;
   end else if (counter == 4'b1111) begin
+  
+    U1 <= U11;
+    U2 <= U12;
+    U3 <= 8'b0;
+    U4 <= U15;
+    U5 <= U16;
+    U6 <= 8'b0;
+    U7 <= 8'b0;
+    U8 <= 8'b0;
+    U9 <= 8'b0;
+
     Y1 <= Y11;
     Y2 <= Y12;
-    Y3 <= 8'b00000000;
+    Y3 <= 8'b0;
     Y4 <= Y15;
     Y5 <= Y16;
-    Y6 <= 8'b00000000;
-    Y7 <= 8'b00000000;
-    Y8 <= 8'b00000000;
-    Y9 <= 8'b00000000;
+    Y6 <= 8'b0;
+    Y7 <= 8'b0;
+    Y8 <= 8'b0;
+    Y9 <= 8'b0;
+    
+    I <= I16;
+    
     Y16_next <= onebyoneout;
   end
   if (counter == 4'b1111) begin
@@ -298,15 +529,15 @@ onebyone u1(
 .B8(B8i[7:0]),
 .B9(B9i[7:0]),
 
-.U1(U1i[7:0]),
-.U2(U2i[7:0]),
-.U3(U3i[7:0]),
-.U4(U4i[7:0]),
-.U5(U5i[7:0]),
-.U6(U6i[7:0]),
-.U7(U7i[7:0]),
-.U8(U8i[7:0]),
-.U9(U9i[7:0]),
+.U1(U1[7:0]),
+.U2(U2[7:0]),
+.U3(U3[7:0]),
+.U4(U4[7:0]),
+.U5(U5[7:0]),
+.U6(U6[7:0]),
+.U7(U7[7:0]),
+.U8(U8[7:0]),
+.U9(U9[7:0]),
 
 .Y1(Y1[8:0]),
 .Y2(Y2[8:0]),
@@ -318,7 +549,7 @@ onebyone u1(
 .Y8(Y8[8:0]),
 .Y9(Y9[8:0]),
 
-.I(Ii[7:0]),
+.I(I[7:0]),
 
 .out(onebyoneout[8:0])
 
