a249a25db737225e4b3e8a48eee5e1dcd5e08215
DreamSoul <diao_em@hotmail.com>
2015-03-04 16:37:15 -0500

    Change A and B to signed

--- Quartus/eq1.v
+++ Quartus/eq1.v
@@ -11,13 +11,13 @@ I,
 
 out
   );
-	input wire [7:0] A1, A2, A3, A4, A5, A6, A7, A8, A9;
-	input wire [7:0] B1, B2, B3, B4, B5, B6, B7, B8, B9;
+	input wire signed [7:0] A1, A2, A3, A4, A5, A6, A7, A8, A9;
+	input wire signed [7:0] B1, B2, B3, B4, B5, B6, B7, B8, B9;
 	input wire [7:0] U1, U2, U3, U4, U5, U6, U7, U8, U9;
 	input wire signed [8:0] Y1, Y2, Y3, Y4, Y5, Y6, Y7, Y8, Y9;
 	wire signed [16:0] outY;
 	wire signed [16:0] outU;
-	input wire [7:0] I;
+	input wire signed [7:0] I;
 	output wire signed [16:0] out;
 	
 	assign outY = (A1 * Y1)+ (A2 * Y2) + (A3 * Y3) + (A4 * Y4) + (A5 * Y5) + (A6 * Y6) + (A7 * Y7) + (A8 * Y8) + (A9 * Y9);
