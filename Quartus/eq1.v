75cd5d30b906d8bdd4cb3ce191a6093d94b844e4
DreamSoul <diao_em@hotmail.com>
2015-03-11 16:25:40 -0400

    debug width

--- Quartus/eq1.v
+++ Quartus/eq1.v
@@ -18,7 +18,7 @@ out
 	wire signed [2 * `WIDTH - 1:0] outY;
 	wire signed [2 * `WIDTH - 1:0] outU;
 	input wire signed [`WIDTH - 1:0] I;
-	output wire signed [2 * (`WIDTH - 1):0] out;
+	output wire signed [2 * `WIDTH - 1:0] out;
 	
 	assign outY = (A1 * Y1)+ (A2 * Y2) + (A3 * Y3) + (A4 * Y4) + (A5 * Y5) + (A6 * Y6) + (A7 * Y7) + (A8 * Y8) + (A9 * Y9);
 	assign outU = (B1 * U1)+ (B2 * U2) + (B3 * U3) + (B4 * U4) + (B5 * U5) + (B6 * U6) + (B7 * U7) + (B8 * U8) + (B9 * U9);
