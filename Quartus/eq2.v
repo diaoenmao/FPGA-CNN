0349f67aa8adbf71b3faad0775a6c0cc95f5faeb
DreamSoul <diao_em@hotmail.com>
2015-03-11 16:26:34 -0400

    debug width to just value 1

--- Quartus/eq2.v
+++ Quartus/eq2.v
@@ -9,9 +9,9 @@ out
 	wire signed [2 * `WIDTH - 1:0] Xminus;
 	output wire signed [`WIDTH - 1:0] out;
 	
-	assign Xplus1 = X + (2 * `WIDTH - 1)'b1;
-	assign Xminus1 = X - (2 * `WIDTH - 1)'b1;	
+	assign Xplus1 = X + 1;
+	assign Xminus1 = X - 1;	
 	assign Xplus = (Xplus1[2 * `WIDTH - 1])?-Xplus1:Xplus1;
 	assign Xminus =  (Xminus1[2 * `WIDTH - 1])?-Xminus1:Xminus1;
-	assign out = (Xplus + Xminus) >>> (2 * `WIDTH - 1)'b1;
+	assign out = (Xplus + Xminus) >>> 1;
 endmodule
