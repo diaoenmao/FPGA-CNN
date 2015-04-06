5bed1b21aaa5196e720ab60259063805165083eb
DreamSoul <diao_em@hotmail.com>
2015-04-06 13:26:05 -0400

    Comment

--- Quartus/StringGenerator.java
+++ Quartus/StringGenerator.java
@@ -5,7 +5,7 @@ public class StringGenerator
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		int size = 16;
+		int size = 16; // change this if need
 		int[] vertical_left = new int[size];
     		for (int i = 1; i < size-1; i++) {
         		vertical_left[i] = i*size+1;
@@ -16,7 +16,7 @@ public class StringGenerator
     		}     		
 		for(int i=1;i<size*size;i++) {
 			// four corners
-			if(i==1) {
+			if(i==1) { // top left
 				System.out.println("\n    Y"+size+"_next <= onebyoneout;\n"
 				+ "\n    U1 <= `WIDTH'b0;"
 				+ "\n    U2 <= `WIDTH'b0;" + "\n    U3 <= `WIDTH'b0;"
@@ -29,7 +29,7 @@ public class StringGenerator
 				+ "\n    Y4 <= {(2 * `WIDTH){1'b0}}" + "\n    Y5 <= Y"+(i)+"_in;"
 				+ "\n    Y6 <= Y"+(i+1)+"_in;" + "\n    Y7 <= {(2 * `WIDTH){1'b0}};"
 				+ "\n    Y8 <= Y"+(i+size)+"_in;" + "\n    Y9 <= Y"+(i+size+1)+"_in");												
-			} else if(i==size) {
+			} else if(i==size) { // top right
 				System.out.println("\n    Y"+(i-1)+"_next <= onebyoneout;\n"
 				+ "\n    U1 <= `WIDTH'b0;"
 				+ "\n    U2 <= `WIDTH'b0;" + "\n    U3 <= `WIDTH'b0;"
@@ -42,7 +42,7 @@ public class StringGenerator
 				+ "\n    Y4 <= Y"+(i-1)+"_in;" + "\n    Y5 <= Y"+(i)+"_in;"
 				+ "\n    Y6 <= {(2 * `WIDTH){1'b0}};" + "\n    Y7 <= Y"+(i+size-1)+"_in;"
 				+ "\n    Y8 <= Y"+(i+size)+"_in;" + "\n    Y9 <= {(2 * `WIDTH){1'b0}}");				
-			} else if(i==size*(size-1)+1) {
+			} else if(i==size*(size-1)+1) { // bottom left
 				System.out.println("\n    Y"+(i-1)+"_next <= onebyoneout;\n"
 				+ "\n    U1 <= `WIDTH'b0;"
 				+ "\n    U2 <= U"+(i-size)+"_in;" + "\n    U3 <= U"+(i-size+1)+"_in;"
@@ -55,7 +55,7 @@ public class StringGenerator
 				+ "\n    Y4 <= {(2 * `WIDTH){1'b0}};" + "\n    Y5 <= Y"+(i)+"_in;"
 				+ "\n    Y6 <= Y"+(i+1)+"_in;" + "\n    Y7 <= {(2 * `WIDTH){1'b0}};"
 				+ "\n    Y8 <= {(2 * `WIDTH){1'b0}};" + "\n    Y9 <= {(2 * `WIDTH){1'b0}};\n");				
-			}else if(i==size*size) {
+			}else if(i==size*size) { // bottom right
 				System.out.println("\n    Y"+(i-1)+"_next <= onebyoneout;\n"
 				+ "\n    U1 <= U"+(i-size-1)+"_in;"
 				+ "\n    U2 <= U"+(i-size)+"_in;" + "\n    U3 <= `WIDTH'b0;"
@@ -67,8 +67,9 @@ public class StringGenerator
 				+ "\n    Y2 <= Y"+(i-size)+"_in;" + "\n    Y3 <= {(2 * `WIDTH){1'b0}};"
 				+ "\n    Y4 <= Y"+(i-1)+"_in;" + "\n    Y5 <= Y"+(i)+"_in;"
 				+ "\n    Y6 <= {(2 * `WIDTH){1'b0}};" + "\n    Y7 <= {(2 * `WIDTH){1'b0}};"
-				+ "\n    Y8 <= {(2 * `WIDTH){1'b0}};" + "\n    Y9 <= {(2 * `WIDTH){1'b0}};\n");				
-			} else if(i>=2&i<=size-1) {
+				+ "\n    Y8 <= {(2 * `WIDTH){1'b0}};" + "\n    Y9 <= {(2 * `WIDTH){1'b0}};\n");	
+			// four boundarier except corners
+			} else if(i>=2&i<=size-1) { // top 
 				System.out.println("\n    Y"+(i-1)+"_next <= onebyoneout;\n"
 				+ "\n    U1 <= `WIDTH'b0;"
 				+ "\n    U2 <= `WIDTH'b0;" + "\n    U3 <= `WIDTH'b0;"
@@ -81,7 +82,7 @@ public class StringGenerator
 				+ "\n    Y4 <= Y"+(i-1)+"_in;" + "\n    Y5 <= Y"+(i)+"_in;"
 				+ "\n    Y6 <= Y"+(i+1)+"_in;" + "\n    Y7 <= Y"+(i+size-1)+"_in;"
 				+ "\n    Y8 <= Y"+(i+size)+"_in;" + "\n    Y9 <= Y"+(i+size+1)+"_in;\n");					
-			} else if(i>=size*(size-1)+2&i<=size*size-1) {
+			} else if(i>=size*(size-1)+2&i<=size*size-1) { // bottom
 				System.out.println("\n    Y"+(i-1)+"_next <= onebyoneout;\n"
 				+ "\n    U1 <= U"+(i-size-1)+"_in;"
 				+ "\n    U2 <= U"+(i-size)+"_in;" + "\n    U3 <= U"+(i-size+1)+"_in;"
@@ -94,7 +95,7 @@ public class StringGenerator
 				+ "\n    Y4 <= Y"+(i-1)+"_in;" + "\n    Y5 <= Y"+(i)+"_in;"
 				+ "\n    Y6 <= Y"+(i+1)+"_in;" + "\n    Y7 <= {(2 * `WIDTH){1'b0}};"
 				+ "\n    Y8 <= {(2 * `WIDTH){1'b0}};" + "\n    Y9 <= {(2 * `WIDTH){1'b0}};\n");					
-			} else if(Arrays.asList(vertical_left).contains(i)) {
+			} else if(Arrays.asList(vertical_left).contains(i)) { // left
 				System.out.println("\n    Y"+(i-1)+"_next <= onebyoneout;\n"
 				+ "\n    U1 <= `WIDTH'b0;"
 				+ "\n    U2 <= U"+(i-size)+"_in;" + "\n    U3 <= U"+(i-size+1)+"_in;"
@@ -107,7 +108,7 @@ public class StringGenerator
 				+ "\n    Y4 <= {(2 * `WIDTH){1'b0}};" + "\n    Y5 <= Y"+(i)+"_in;"
 				+ "\n    Y6 <= Y"+(i+1)+"_in;" + "\n    Y7 <= {(2 * `WIDTH){1'b0}};"
 				+ "\n    Y8 <= Y"+(i+size)+"_in;" + "\n    Y9 <= Y"+(i+size+1)+"_in;\n");					
-			} else if(Arrays.asList(vertical_right).contains(i)) {
+			} else if(Arrays.asList(vertical_right).contains(i)) { // right
 				System.out.println("\n    Y"+(i-1)+"_next <= onebyoneout;\n"
 				+ "\n    U1 <= U"+(i-size-1)+"_in;"
 				+ "\n    U2 <= U"+(i-size)+"_in;" + "\n    U3 <= `WIDTH'b0;"
@@ -120,7 +121,7 @@ public class StringGenerator
 				+ "\n    Y4 <= Y"+(i-1)+"_in;" + "\n    Y5 <= Y"+(i)+"_in;"
 				+ "\n    Y6 <= {(2 * `WIDTH){1'b0}};" + "\n    Y7 <= Y"+(i+size-1)+"_in;"
 				+ "\n    Y8 <= Y"+(i+size)+"_in;" + "\n    Y9 <= {(2 * `WIDTH){1'b0}};"	);			
-			} else {
+			} else { // all other cells in the middle
 				System.out.println("\n    Y"+(i-1)+"_next <= onebyoneout;\n"
 				+ "\n    U1 <= U"+(i-size-1)+"_in;"
 				+ "\n    U2 <= U"+(i-size)+"_in;" + "\n    U3 <= U"+(i-size+1)+"_in;"
